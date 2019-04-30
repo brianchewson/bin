bdcb2bbc0c75b773f3301cb7938c3c2ff9c7c448
Brian Hewson <brian.hewson@cd-adapco.com>
2019-04-30 16:13:43 -0400

    updates for abandoned project logroller

--- logRoller.sh
+++ logRoller.sh
@@ -1,22 +1,26 @@
-#!/bin/bash 
+#!/bin/sh -e
 
-INFILE=$1
-SIZE_NOW=$(stat -c%s $INFILE)
-SIZE_THEN=0
-WAIT=0
-TIMEOUT=1
+#============================================FUNCTIONS==============================================
+usage()
+{
+  echo "$0 is a tool to watch the logs in a directory, tailing the most recent
+-----------------------------------------------------------------------bch
+USAGE: $0 -i FILENAME
+  -i     #The filename of the first log you'd like to tail.
+"
+  echo $*
+  exit 1
+}
 
-CLEAR_VAL=$(uname)
-if [ $CLEAR_VAL = "Linux" ]; then
-  CLEAR_VAL='tput reset'
-else
-  CLEAR_VAL='echo -e "\ec\e[3J"'
-fi
+get_new_log()
+{
+  
+}
 
 set_sizes()
 {
   SIZE_THEN=$SIZE_NOW
-  SIZE_NOW=$(stat -c%s $INFILE)  
+  SIZE_NOW=$(stat -c%s $INFILE)
 }
 
 show_log()
@@ -25,27 +29,52 @@ show_log()
   tail $INFILE
 }
 
-get_new_log()
+process_arguments()
 {
-  local LOG_LOCATION=$INFILE
-  local RESULTS_DIR=${LOG_LOCATION%/testrun*}
-  local TIMESTAMP=${RESULTS_DIR##*/}
-  
-  local LASTTIME=$(\ls -1 ${RESULTS_DIR%/*}|tail -1)
-
-  #when watching logs on a run of startest, the timestamp value will not be a number.
-  #if that is the case, then make sure that the LASTTIME and TIMESTAMP values are not equal so the
-  #log iterates
-  case $TIMESTAMP in
-    [a-Z]*)
-      TIMESTAMP=ARCH-COMPILER 
-    ;;
-  esac
-  if [ ! "$TIMESTAMP" = "$LASTTIME" ]; then
-    INFILE=$(ls -1tr ${RESULTS_DIR%/*}/${LASTTIME}/testrun*/logs/*.log | tail -1)
-    TIMEOUT=120
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -h|--help)
+        usage
+      ;;
+      -i|-I)
+        if [ -z "$2" ]; then
+          usage "Improper number of arguments supplied for FILENAME flag (-i)"
+        fi
+        INFILE=$2
+        shift
+      ;;
+    esac
+    shift
+  done
+  if [ -z "$INFILE" ]; then
+    usage "No FILE specified"
+  fi
+  if [ ! -f "$INFILE" ]; then
+    usage "File $INFILE doesn't exist"
   fi
 }
+#==========================================END FUNCTIONS============================================
+
+INFILE=""
+
+CLEAR_VAL=$(uname)
+if [ $CLEAR_VAL = "Linux" ]; then
+  CLEAR_VAL='tput reset'
+else
+  CLEAR_VAL='echo -e "\ec\e[3J"'
+fi
+SIZE_NOW=0
+SIZE_THEN=0
+TIMEOUT=1
+WAIT=0
+
+if [ $# -lt 1 ]; then
+  usage "No arguments specified"
+fi
+
+process_arguments "$@"
+INFILE=$(readlink -f ${INFILE})
+set_sizes
 
 while [ 1 ];
 do
@@ -60,14 +89,14 @@ do
     if [ $SIZE_THEN -eq $SIZE_NOW ]; then
       show_log
       echo "NO CHANGE...WAITING ${WAIT}(s) FOR CHANGE..."
-      sleep 1 
+      sleep 1
       WAIT=$(($WAIT + 1))
     else
       show_log
       WAIT=0
-      TIMEOUT=120
     fi
   fi
 
   set_sizes
 done
+
