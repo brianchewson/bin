e21934e60cde7ba2e1254fad924f4fa0229134f7
Brian Hewson <brian.hewson@cd-adapco.com>
2021-10-20 16:16:00 -0400

    Require a filename, cleanup after yourself

--- make_iso.sh
+++ make_iso.sh
@@ -5,6 +5,11 @@ DATE=$(date +%Y-%m-%d-%H%M%s)
 INFILE=$1
 ISO=${INFILE%.xml}.iso
 
+if [ ! -f "${INFILE}" ]; then
+    echo "You didn't specify a file to turn into an iso"
+    exit
+fi
+
 cp ${INFILE} ${AUTO_INST}
 
 mkisofs -V OEMDRV -o ${ISO} ${AUTO_INST}
@@ -15,4 +20,7 @@ scp ${ISO} root@images:/srv/images/SUSE/${ISO}
 
 if [ -d backups ]; then
   mv ${ISO} backups/${DATE}-${ISO}
+else
+    echo "Deleting ISO"
+    rm -f ${ISO}
 fi
